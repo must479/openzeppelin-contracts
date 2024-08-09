55217ce2d93c3b51f4cd710b6f5553f4d93d4275
Must479 <67345599+must479@users.noreply.github.com>
2024-08-09 20:05:48 +0700

    Update git-user-config.sh

--- scripts/git-user-config.sh
+++ scripts/git-user-config.sh
@@ -3,4 +3,4 @@
 set -euo pipefail -x
 
 git config user.name 'github-actions'
-git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
+git config user.email '67345599+github-actions[bot]@users.noreply.github.com'
